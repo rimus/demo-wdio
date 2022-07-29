4040ab256ed38694bbca00dbd579b32cf281630d
Aliaksandr Yeutushkou <Aliaksandr_Yeutushkou@epam.com>
2022-07-29 15:48:00 +0300

    Fix naming issues

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,12 +1,12 @@
-const HomePage = require('../../app/page-objects/home-page');
-const LoginPage = require('../../app/page-objects/login-page');
-const AccountPage = require('../../app/page-objects/account-page');
+const homePage = require('../../app/page-objects/home-page');
+const loginPage = require('../../app/page-objects/login-page');
+const accountPage = require('../../app/page-objects/account-page');
 
 describe('Awesome shop', () => {
   it('should login with valid credentials', async () => {
-    await HomePage.open();
-    await HomePage.goToLoginPage();
-    await LoginPage.login('foobar@foo.bar', '123456');
-    await expect(await AccountPage.editAccountLink).toBeDisplayed();
+    await homePage.open();
+    await homePage.goToLoginPage();
+    await loginPage.login('foobar@foo.bar', '123456');
+    await expect(await accountPage.editAccountLink).toBeDisplayed();
   });
 });
